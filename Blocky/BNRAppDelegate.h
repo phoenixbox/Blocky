130e39fa906a2140431ff3b1c1a59aaa614b4209
phoenixbox <shane@quickleft.com>
2013-12-04 08:28:29 -0700

    Initial Commit

---
+++ Blocky/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Blocky
+//
+//  Created by Shane Rogers on 12/4/13.
+//  Copyright (c) 2013 Shane Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlockyTests/BlockyTests.m
@@ -0,0 +1,34 @@
+//
+//  BlockyTests.m
+//  BlockyTests
+//
+//  Created by Shane Rogers on 12/4/13.
+//  Copyright (c) 2013 Shane Rogers. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BlockyTests : XCTestCase
+
+@end
+
+@implementation BlockyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
